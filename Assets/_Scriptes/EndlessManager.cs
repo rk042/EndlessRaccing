b65ad21fc39479ae3090ee34da031801d31085cf
rathod ketan <55497156+rk042@users.noreply.github.com>
2021-08-01 16:24:42 +0530

    URP Upgrade
    - add lights, adds normal maps

--- Assets/_Scriptes/EndlessManager.cs
+++ Assets/_Scriptes/EndlessManager.cs
@@ -9,7 +9,7 @@ public class EndlessManager : MonoBehaviour
 
     public SpriteShapeController ground;
     public float offset;
-   
+    public List<GameObject> _treesList = new List<GameObject>(); 
     
     // Start is called before the first frame update
     void Start()
@@ -28,8 +28,17 @@ public class EndlessManager : MonoBehaviour
             float posx = ground.spline.GetPosition(i + 1).x + 10f;
 
             ground.spline.InsertPointAt(i + 2, new Vector3(posx, Random.Range(0, 2f)));
+
+            Instantiate(_treesList[Random.Range(0, _treesList.Count)],new Vector2(ground.spline.GetPosition(i).x, ground.spline.GetPosition(i).y),Quaternion.identity);
         }
-            
+
+        //for (int i = 2; i < 500; i++)
+        //{
+        //    ground.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
+        //    ground.spline.SetLeftTangent(i, new Vector3(-1, 0, 0));
+        //    ground.spline.SetRightTangent(i, new Vector3(1, 0, 0));
+                
+        //}
     }
 
     // Update is called once per frame
